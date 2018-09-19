4d46b6eb19ceb7e5386ff2aafa5b1e17ac90a898
retrofuturejosh <joshuadsohn@gmail.com>
2018-09-19 16:08:20 -0400

    [fixes] script and greengrass dependencies

--- src/greengrassSetUp.js
+++ src/greengrassSetUp.js
@@ -1,6 +1,6 @@
 const AWS = require(`aws-sdk`);
 const iot = new AWS.Iot({ apiVersion: '2015-05-28', region: 'us-east-1' });
-const fs = 'fs';
+const fs = require('fs');
 
 const greengrass = new AWS.Greengrass({
   apiVersion: '2017-06-07',
