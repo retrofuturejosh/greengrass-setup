061d4025cfa3ab2a835b85b213ac33027bbc51ab
retrofuturejosh <joshuadsohn@gmail.com>
2018-09-20 17:31:15 -0400

    small debug

--- src/scripts/start_greengrass.sh
+++ src/scripts/start_greengrass.sh
@@ -8,7 +8,7 @@ adduser --system ggc_user
 groupadd --system ggc_group
 
 # get CA root
-wget -O $CWD/certs/root.ca.pem http://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem
+wget -O $CWD/certs/root-ca-pem http://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem
 
 # Copy greengrass binaries
 tar -xzf $CWD/downloads/greengrass-linux-x86-64-1.6.0.tar.gz -C /
@@ -17,7 +17,7 @@ tar -xzf $CWD/downloads/greengrass-linux-x86-64-1.6.0.tar.gz -C /
 cp $CWD/certs/* /greengrass/certs
 
 # Copy config
-cp $CWD/config.json /greengrass/config
+/bin/cp $CWD/config.json /greengrass/config
 
 # Start greengrass
 cd /greengrass/ggc/core
