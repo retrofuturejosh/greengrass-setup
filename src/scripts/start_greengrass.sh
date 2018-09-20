2746dafc096d55987b7b4e07cf028a1edd4004b9
retrofuturejosh <joshuadsohn@gmail.com>
2018-09-20 17:06:16 -0400

    [fixes] paths in config.json

--- src/scripts/start_greengrass.sh
+++ src/scripts/start_greengrass.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
+sudo su
+
+export CWD=$(pwd)
 
 # create gg users
 adduser --system ggc_user
 groupadd --system ggc_group
 
 # get CA root
-wget -O /home/ec2-user/iot-workshop/greengrass-setup/certs/root.ca.pem http://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem
+wget -O $CWD/certs/root.ca.pem http://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem
 
 # Copy greengrass binaries
-tar -xzf /home/ec2-user/iot-workshop/greengrass-setup/downloads/greengrass-linux-x86-64-1.6.0.tar.gz -C /
+tar -xzf $CWD/downloads/greengrass-linux-x86-64-1.6.0.tar.gz -C /
 
 # Copy certs
-sudo cp /home/ec2-user/iot-workshop/greengrass-setup/certs/* /greengrass/certs
+cp $CWD/certs/* /greengrass/certs
 
 # Copy config
-sudo cp /home/ec2-user/iot-workshop/greengrass-setup/config.json /greengrass/config
+cp $CWD/config.json /greengrass/config
 
+# Start greengrass
+cd /greengrass/ggc/core
+./greengrassd start
